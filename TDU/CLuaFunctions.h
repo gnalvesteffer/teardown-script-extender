33dc70bcc4bf16f0bd51bc58043b709d97d212e1
nxhu64 <nxhu64@gmail.com>
2021-03-26 23:27:23 -0300

    Added Vox functions

--- TDU/CLuaFunctions.h
+++ TDU/CLuaFunctions.h
@@ -8,5 +8,12 @@
 
 namespace CLuaFunctions
 {
+	namespace EntityFunctions
+	{
+		int CreateShape(lua_State* L);
+		int CreateBody(lua_State* L);
+		int LoadVox(lua_State* L);
+	}
+
 	void RegisterCFunctions(lua_State* L);
 }
